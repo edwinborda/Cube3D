9d6569b818c21ec509d90a51499c09f92c4f74d4
EdwinBorda <edwin.borda@outlook.com>
2016-07-22 15:35:17 -0500

    Se crean las entidades POCO del sistema

---
+++ Cube3DSum/Cube3D.Entity/DetalleMatriz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cube3D.Entity
+{
+    [Table("DetalleMatriz")]
+    public class DetalleMatriz
+    {
+        public int Id { get; set; }
+
+        public int MatrizId { get; set; }
+
+        public string Coordenada { get; set; }
+
+        [Range(0,)]
+        public int Valor { get; set; }
+
+    }
+}
---
+++ Cube3DSum/Cube3D.Entity/Matriz.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cube3D.Entity
+{
+    [Table("DetalleMatriz")]
+    public class Matriz
+    {
+        public int Id { get; set; }
+
+        [Range(0,50)]
+        public int TamMatriz { get; set; }
+
+        [Range(0,50)]
+        public int NumOperaciones { get; set; }
+    }
+}
